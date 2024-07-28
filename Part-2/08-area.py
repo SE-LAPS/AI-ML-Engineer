42be0baa0c6f8bab56000f17239a9dbb21421035
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-07-28 11:58:26 +0530

    Delete 08-area.py

--- Part-2/08-area.py
+++
@@ -1,8 +0,0 @@
-length = int(input("Enter Length "))
-width = int(input("Enter Width "))
-
-area = length * width
-perimeter = (2*length) + (2*width)
-
-print ("Area is ", area)
-print ("Perimeter is ", perimeter)
